5ad300522e3b8037da15ecc4e941c6f00e7dc407
bora <nhatbaoxxd@gmail.com>
2024-02-03 15:54:43 +0700

    Other useMutation Properties

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 import { fetchPosts, deletePost, updatePost } from '@/api'
 import { PostDetail } from '@/PostDetail'
@@ -17,6 +17,13 @@ export function Posts() {
     queryKey: ['posts', currentPage],
     queryFn: () => fetchPosts(currentPage),
     staleTime: 5000,
+  })
+
+  const deleteMutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: () => {
+      queryClient.invalidateQueries('posts')
+    },
   })
 
   useEffect(() => {
@@ -38,7 +45,14 @@ export function Posts() {
     <>
       <ul>
         {data.map((post) => (
-          <li key={post.id} className="post-title" onClick={() => setSelectedPost(post)}>
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => {
+              deleteMutation.reset()
+              setSelectedPost(post)
+            }}
+          >
             {post.title}
           </li>
         ))}
@@ -53,7 +67,7 @@ export function Posts() {
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && <PostDetail post={selectedPost} deleteMutation={deleteMutation} />}
     </>
   )
 }
