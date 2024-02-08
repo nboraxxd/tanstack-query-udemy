5466adef50a2d2caadd47b75a9686d5c99a039a1
bora <nhatbaoxxd@gmail.com>
2024-02-08 12:33:53 +0700

    useAppointments

--- base-lazy-days/client/src/components/appointments/Calendar.tsx
+++ base-lazy-days/client/src/components/appointments/Calendar.tsx
@@ -1,24 +1,29 @@
-import {
-  Box,
-  Checkbox,
-  Grid,
-  Heading,
-  HStack,
-  IconButton,
-} from "@chakra-ui/react";
-import dayjs from "dayjs";
-import { TiArrowLeftThick, TiArrowRightThick } from "react-icons/ti";
+import { Box, Checkbox, Grid, Heading, HStack, IconButton } from '@chakra-ui/react'
+import { useQueryClient } from '@tanstack/react-query'
+import dayjs from 'dayjs'
+import { useEffect } from 'react'
+import { TiArrowLeftThick, TiArrowRightThick } from 'react-icons/ti'
 
-import { DateBox } from "./DateBox";
-import { useAppointments } from "./hooks/useAppointments";
-
-import { UserAppointments } from "@/components/user/UserAppointments";
+import { DateBox } from '@/components/appointments/DateBox'
+import { getNewMonthYear } from '@/components/appointments/hooks/monthYear'
+import { getAppointments, useAppointments } from '@/components/appointments/hooks/useAppointments'
+import { UserAppointments } from '@/components/user/UserAppointments'
+import { queryKeys } from '@/react-query/constants'
 
 export function Calendar() {
-  const currentDate = dayjs();
+  const queryClient = useQueryClient()
+  const currentDate = dayjs()
 
-  const { appointments, monthYear, updateMonthYear, showAll, setShowAll } =
-    useAppointments();
+  const { appointments, monthYear, updateMonthYear, showAll, setShowAll } = useAppointments()
+
+  useEffect(() => {
+    const nextMonth = getNewMonthYear(monthYear, 1)
+
+    queryClient.prefetchQuery({
+      queryKey: [queryKeys.appointments, nextMonth.year, nextMonth.month],
+      queryFn: () => getAppointments(nextMonth.year, nextMonth.month),
+    })
+  }, [monthYear, queryClient])
 
   return (
     <Box>
@@ -32,11 +37,7 @@ export function Calendar() {
         <Heading minW="40%" textAlign="center">
           {monthYear.monthName} {monthYear.year}
         </Heading>
-        <IconButton
-          aria-label="next month"
-          onClick={() => updateMonthYear(1)}
-          icon={<TiArrowRightThick />}
-        />
+        <IconButton aria-label="next month" onClick={() => updateMonthYear(1)} icon={<TiArrowRightThick />} />
         <Checkbox
           variant="flushed"
           width="48"
@@ -51,19 +52,13 @@ export function Calendar() {
       </HStack>
       <Grid templateColumns="repeat(7, 1fr)" gap={4} my={5} mx={10}>
         {/* first day needs a grid column */}
-        <DateBox
-          date={1}
-          gridColumn={monthYear.firstDOW + 1}
-          appointments={appointments[1]}
-        />
+        <DateBox date={1} gridColumn={monthYear.firstDOW + 1} appointments={appointments[1]} />
         {/* the rest of the days will follow */}
         {[...Array(monthYear.lastDate)].map((_, i) =>
-          i > 0 ? (
-            <DateBox key={i} date={i + 1} appointments={appointments[i + 1]} />
-          ) : null
+          i > 0 ? <DateBox key={i} date={i + 1} appointments={appointments[i + 1]} /> : null
         )}
       </Grid>
       <UserAppointments />
     </Box>
-  );
+  )
 }
